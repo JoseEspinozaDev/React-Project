45d51773fb82ea8fac7264f6ef91bfed2420e55e
JoseDev7 <josefr7h@gmail.com>
2025-11-03 07:56:53 -0300

    Add compontent to app.js

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import Products from "./components/Productos";
 import showText from "./components/ShowDataRealTime";
 import ShowDataRealTime from "./components/ShowDataRealTime";
 import ModeDark from "./components/modeDark";
+import TwitterCard from "./components/TwitterCard";
 
 function App() {
   return (
@@ -15,19 +16,7 @@ function App() {
       <Header />
        <Saludar nombre='Juan Perez'/>
       <div className="container mt-4">
-        <Products/>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Button nombre='ENVIAR DATOS'/>
-        <Button nombre='RECIBIR DATOS'/>
-        <Button nombre='CANCELAR DATOS'/>
-        <Button nombre='IMPRIMIR FACTURA'/>
-        <Button nombre='EMITIR FACTURA'/>
-        <Contador/>
-        <ShowDataRealTime/>
-        <ModeDark/>
+        <TwitterCard/>
       </div>
 
       
