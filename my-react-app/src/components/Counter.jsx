c3e15a11d8b6de01371142c3735b307811470575
JoseDev7 <josefr7h@gmail.com>
2025-11-03 07:37:25 -0300

    Crear rama develope para desarrollo con los nuevos archivos

--- my-react-app/src/components/Counter.jsx
+++ my-react-app/src/components/Counter.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function Contador() {
+
+    
   const [contador, setContador] = useState(()=> {
     const valorGuardado = localStorage.getItem('contador')
     return valorGuardado ? JSON.parse(valorGuardado): 0
@@ -15,6 +17,8 @@ function Contador() {
   useEffect(() => {
     localStorage.setItem("contador", JSON.stringify(contador));
   }, [contador]);
+
+
   return (
     <div className="container">
       <button onClick={incrementar} className="btn btn-primary m-2">
---
+++ my-react-app/src/components/TwitterCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TwitterCard(){
+
+    return(
+        <div>
+            <h1>Twitter Card</h1>
+        </div>
+    )
+
+}
+
+export default TwitterCard
